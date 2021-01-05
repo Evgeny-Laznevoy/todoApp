d98b86b14eb1cde1fbf869a1dd554a177e3c0f0c
Evgeny-Laznevoy <laznevoy86@gmail.com>
2021-01-05 19:09:41 +0500

    adding feature for delete completed tastks

--- src/App.vue
+++ src/App.vue
@@ -5,53 +5,30 @@
         class="flex text-center"
         v-text="`${name}'s To Do List`"
       ></v-toolbar-title>
-      <!-- <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn> -->
     </v-app-bar>
-
+    
     <v-main>
       <v-container>
-        <v-row>
-          <v-col class="font-weight-bold">Task</v-col>
-          <v-col cols="2" class="font-weight-bold">Done</v-col>
-        </v-row>
-        <v-row v-for="task in filteredTasks" :key="task.action">
-          <v-col>{{ task.action }}</v-col>
-          <v-col cols="2">
-            <v-checkbox
-              v-model="task.done"
-              :label="`${task.done}`"
-            ></v-checkbox>
-          </v-col>
-        </v-row>
+        <div v-if="filteredTasks.length == 0">
+          <div class="text-center">
+            <b>Nithing to do. Hurrah!</b>
+          </div>
+        </div>
+        <template v-else>
+          <v-row>
+            <v-col class="font-weight-bold">Task</v-col>
+            <v-col cols="2" class="font-weight-bold">Done</v-col>
+          </v-row>
+          <v-row v-for="task in filteredTasks" :key="task.action">
+            <v-col>{{ task.action }}</v-col>
+            <v-col cols="2">
+              <v-checkbox
+                v-model="task.done"
+                :label="`${task.done}`"
+              ></v-checkbox>
+            </v-col>
+          </v-row>
+        </template>
         <v-row cols="12">
           <v-col cols="10">
             <v-text-field
@@ -67,7 +44,7 @@
             >
           </v-col>
         </v-row>
-        <v-row class="blue-grey">
+        <v-row align="center" justify="center" class="flex blue-grey">
           <v-col>
             <v-checkbox
               color="secondary"
@@ -75,21 +52,24 @@
               :label="`Hide completed tasks`"
             ></v-checkbox>
           </v-col>
+          <v-col>
+            <v-btn @click="deleteCompleted">
+              Delete Completed
+            </v-btn>
+          </v-col>
         </v-row>
       </v-container>
-      <!-- <HelloWorld/> -->
     </v-main>
   </v-app>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld';
 
 export default {
   name: "App",
 
   components: {
-    // HelloWorld,
+
   },
 
   data: () => ({
@@ -121,11 +101,18 @@ export default {
         this.newItemText = "";
       }
     },
+    sorteData(){
+      localStorage.setItem('todos', JSON.stringify(this.tasks))
+    },
+    deleteCompleted(){
+      this.tasks = this.tasks.filter(t => !t.done)
+      this.sorteData()
+    }
   },
   created() {
-    let data = localStorage.getItem("todos")
+    let data = localStorage.getItem("todos");
     if (data != null) {
-      this.tasks = JSON.parse(data)
+      this.tasks = JSON.parse(data);
     }
   },
 };
